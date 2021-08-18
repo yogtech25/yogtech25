c1442ea530945bb8f6e215dad3dcdccd806ff504
yogtech <yogtechdev256@gmail.com>
2021-08-18 22:35:35 +0530

    Update unit test cases

--- iOSCaseStudy/iOSCaseStudyTests/CellITuneSearchListTests.swift
+++ iOSCaseStudy/iOSCaseStudyTests/CellITuneSearchListTests.swift
@@ -1,8 +1,41 @@
-//
-//  CellITuneSearchListTests.swift
-//  iOSCaseStudyTests
-//
-//  Created by Apple on 18/08/21.
+//  Created by rails on 18/08/21.
 //
 
 import Foundation
+import XCTest
+@testable import iOSCaseStudy
+
+class CellITuneSearchListTests: XCTestCase {
+    
+    var cell: CellITuneSearchList!
+    var nameLabel = UILabel()
+    var image = UIImageView()
+    
+    override func setUpWithError() throws {
+        cell = CellITuneSearchList()
+        cell.nameLabel = nameLabel
+        cell.thumbnailImage = image
+        image.frame = cell.thumbnailImage.frame
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func test_nameLabel() {
+        cell.setUpData(model: WebData(trackId: 454545, trackName: "New Songs", artistName: "Donald", artworkUrl60: SDConstant().staticImageUrl))
+        XCTAssert(cell.nameLabel.text! == "New Songs", "")
+    }
+}
--- iOSCaseStudy/iOSCaseStudyTests/ITuneSearchListViewControllerTests.swift
+++ iOSCaseStudy/iOSCaseStudyTests/ITuneSearchListViewControllerTests.swift
@@ -11,24 +11,43 @@ import XCTest
 
 class ITuneSearchListViewControllerTests: XCTestCase {
     var vc: ITuneSearchListViewController!
+    var tableView = UITableView()
     
     override func setUpWithError() throws {
         vc = ITuneSearchListViewController()
+        vc.tableView = tableView
+        vc.webListData? = [WebData(trackId: 454545, trackName: "New Songs", artistName: "Donald", artworkUrl60: "")]
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testTableViewOutlet() {
+        XCTAssertNotNil(vc.tableView)
+    }
+    
+    func test_whenRegisteredCellInTableview() {
+        vc.tableView.register(UINib(nibName: SDConstant().cellITuneSearchList, bundle: nil), forCellReuseIdentifier: SDConstant().cellITuneSearchList)
+        vc.tableView.reloadData()
+    }
+    
+    func test_addObjectInWebArray() {
+        vc.tableView.register(UINib(nibName: SDConstant().cellITuneSearchList, bundle: nil), forCellReuseIdentifier: SDConstant().cellITuneSearchList)
+        vc.webListData?.append(WebData(trackId: 454545, trackName: "New Songs", artistName: "Donald", artworkUrl60: ""))
+        vc.tableView.reloadData()
+    }
 }
+
