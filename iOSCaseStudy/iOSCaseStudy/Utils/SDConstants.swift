c00117bad34e0762d2be60507f6be1d6f814899e
yogtech <yogtechdev256@gmail.com>
2021-08-18 14:27:32 +0530

    Manage delegate and add constant file

---
+++ iOSCaseStudy/iOSCaseStudy/Utils/SDConstants.swift
@@ -0,0 +1,13 @@
+
+//  Created by rails on 18/08/21.
+//
+
+import Foundation
+
+class SDConstant{
+    let searchUrl                     = "https://itunes.apple.com/search?term=jack+johnson"
+    let detailViewController          = "DetailViewController"
+    let cellITuneSearchList           = "CellITuneSearchList"
+    let deleteAlertMessage            = "Are you sure want to delete this detail"
+    let deleteMessage                 = "Are you sure want to delete this detail"
+}
